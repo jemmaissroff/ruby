526e40e4d4d0b0876b2db13e76d6052a6a3ebaaf
Jemma Issroff <jemmaissroff@gmail.com>
2022-11-01 15:01:21 -0400

    Fix yjit bindings

--- yjit/src/cruby_bindings.inc.rs
+++ yjit/src/cruby_bindings.inc.rs
@@ -420,6 +420,7 @@ pub struct rb_shape {
     pub edges: *mut rb_id_table,
     pub edge_name: ID,
     pub next_iv_index: attr_index_t,
+    pub capacity: u32,
     pub type_: u8,
     pub parent_id: shape_id_t,
 }
