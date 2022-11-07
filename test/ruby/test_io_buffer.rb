0c216d1ed46883c8c771c0fa971c4e87a3f34a40
Jemma Issroff <jemmaissroff@gmail.com>
2022-11-07 17:37:24 -0500

    Making the io buffer test embedded

--- test/ruby/test_io_buffer.rb
+++ test/ruby/test_io_buffer.rb
@@ -331,7 +331,7 @@ class TestIOBuffer < Test::Unit::TestCase
 
   def test_read
     io = Tempfile.new
-    io.write("Hello World")
+    io.write("Hello W")
     io.seek(0)
 
     buffer = IO::Buffer.new(128)
